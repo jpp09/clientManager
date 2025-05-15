3629559c5c655ae81b0674c98b3e93906682e025
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2025-05-15 20:33:53 -0300

    Criando a base de um projeto spring boot

---
+++ src/test/kotlin/br/com/martins/clientManager/ClientManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.martins.clientManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
